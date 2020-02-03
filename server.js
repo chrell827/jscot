eb7ef45aa224438064f07491b2ea4c0d9f8b28bb
Christian <chrell827@gmail.com>
2020-02-02 23:31:49 -0500

    fixing for deployment

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/27017";
 mongoose.connect(MONGODB_URI);
 
 // Routes
